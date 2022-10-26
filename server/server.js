9324347b88eb1481022438dcf88013aa0f758d7b
Phillip <phillip.besse@gmail.com>
2022-10-26 13:46:36 -0400

    fix error in server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const path = requite('path');
+const path = require('path');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
