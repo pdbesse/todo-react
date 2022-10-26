c473a2f098fdd25f72214a227edcc8ecc3db1a9e
Phillip <phillip.besse@gmail.com>
2022-10-26 13:14:50 -0400

    configure server.js with Apollo GraphQL

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const path = requite('path');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/conneciton');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+});
+
+const app = express();
+const PORT = process.env.POORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+};
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const startApolloServer = async () => {
+    await server.start();
+    server.applyMiddleware({ app });
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`Server now running on port ${PORT}.`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        });
+    });
+};
+
+startApolloServer(typeDefs, resolvers);
