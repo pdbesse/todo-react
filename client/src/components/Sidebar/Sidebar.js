c3239a47dcdd1b27a344b44e434745e6d88104aa
Phil-Laptop <phillip.besse@gmail.com>
2022-11-02 14:23:03 -0400

    update Sidebar

---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,50 @@
+import React from "react";
+// import { FaUserCircle } from "react-icons/fa";
+import { FiCheckSquare } from "react-icons/fi";
+import "./Sidebar.css";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="menu">
+        <ul>
+          {/* <li>
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip id={`tooltip-bottom`}>Home</Tooltip>}
+            >
+              <a href="/" className="header_a">
+                <AiFillHome />
+              </a>
+            </OverlayTrigger>
+          </li> */}
+          {/* <li>
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip id={`tooltip-bottom`}>Profile</Tooltip>}
+            >
+              <a href="/profile" className="header_a">
+                <FaUserCircle />
+              </a>
+            </OverlayTrigger>
+          </li> */}
+          <li>
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip id={`tooltip-bottom`}>Matches</Tooltip>}
+            >
+              <a href="/todo" className="header_a">
+                <FiCheckSquare />
+              </a>
+            </OverlayTrigger>
+          </li>
+        </ul>
+      </div>
+      <div className="vertical"></div>
+    </>
+  );
+};
+
+export default Sidebar;
