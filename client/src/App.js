94e4e4a65d132c948d63fbecdd57db3d058cd0a9
Phil-Laptop <phillip.besse@gmail.com>
2022-11-03 10:40:35 -0400

    reorder imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import React from 'react';
+import './App.css';
 import Auth from "./utils/auth";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
