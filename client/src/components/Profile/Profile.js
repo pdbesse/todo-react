331c2a25c2d9f6ea0faa0af1a95c12263be6052e
Phillip <phillip.besse@gmail.com>
2022-11-03 15:37:43 -0400

    update Profile.js

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -12,12 +12,13 @@ import Loader from "../Loader/Loader";
 
 const Profile = () => {
   const { _id: userParam } = useParams();
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { id: userParam },
-  });
-//   console.log({ loading, data });
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME,
+    {
+      variables: { _id: userParam },
+    });
+  console.log({ loading, data });
   const user = data?.me?.profile || data?.user?.profile || {};
-  console.log(data);
+  // console.log(data);
 
   if (loading) {
     return <Loader />;
@@ -26,63 +27,63 @@ const Profile = () => {
   if (!user) {
     return <h4>Please login.</h4>;
   }
-//   let interests = user.interest;
-//   let images = user.images;
+  //   let interests = user.interest;
+  //   let images = user.images;
 
-//   const renderProfileInfo = () => {
-//     return (
-//       <>
-//         <h3 className="">About Me</h3>
-//         <p>{user.aboutme}</p>
-//         <h3 className="">Interests</h3>
-//         <div>
-//           <InterestList interests={interests} />
-//         </div>
-//         <div style={{ margin: "10px 0 " }}>
-//           <a
-//             href={`https://github.com/${user.github}`}
-//             target="_blank"
-//             rel="noreferrer noopener"
-//             className="container profileLinkButtons"
-//           >
-//             <AiOutlineGithub size={40} />
-//           </a>
-//           <a
-//             href={`https://www.linkedin.com/in/${user.linkedin}`}
-//             target="_blank"
-//             rel="noreferrer noopener"
-//             className="container profileLinkButtons"
-//           >
-//             <AiOutlineLinkedin size={40} />
-//           </a>
-//         </div>
-//       </>
-//     );
-//   };
-//   const renderImages = () => {
-//     return (
-//       <ResponsiveMasonry
-//         columnsCountBreakPoints={{
-//           350: 3,
-//           750: 3,
-//           900: 2,
-//           1200: 3,
-//           2100: 4,
-//         }}
-//       >
-//         <Masonry className="masonry_grid">
-//           {images.map((image, i) => (
-//             <img
-//               key={i}
-//               src={image}
-//               style={{ width: "95%", margin: "8px", borderRadius: "8px" }}
-//               alt=""
-//             />
-//           ))}
-//         </Masonry>
-//       </ResponsiveMasonry>
-//     );
-//   };
+  //   const renderProfileInfo = () => {
+  //     return (
+  //       <>
+  //         <h3 className="">About Me</h3>
+  //         <p>{user.aboutme}</p>
+  //         <h3 className="">Interests</h3>
+  //         <div>
+  //           <InterestList interests={interests} />
+  //         </div>
+  //         <div style={{ margin: "10px 0 " }}>
+  //           <a
+  //             href={`https://github.com/${user.github}`}
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className="container profileLinkButtons"
+  //           >
+  //             <AiOutlineGithub size={40} />
+  //           </a>
+  //           <a
+  //             href={`https://www.linkedin.com/in/${user.linkedin}`}
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className="container profileLinkButtons"
+  //           >
+  //             <AiOutlineLinkedin size={40} />
+  //           </a>
+  //         </div>
+  //       </>
+  //     );
+  //   };
+  //   const renderImages = () => {
+  //     return (
+  //       <ResponsiveMasonry
+  //         columnsCountBreakPoints={{
+  //           350: 3,
+  //           750: 3,
+  //           900: 2,
+  //           1200: 3,
+  //           2100: 4,
+  //         }}
+  //       >
+  //         <Masonry className="masonry_grid">
+  //           {images.map((image, i) => (
+  //             <img
+  //               key={i}
+  //               src={image}
+  //               style={{ width: "95%", margin: "8px", borderRadius: "8px" }}
+  //               alt=""
+  //             />
+  //           ))}
+  //         </Masonry>
+  //       </ResponsiveMasonry>
+  //     );
+  //   };
   return (
     <Row className="profileContainer">
       <Row className="profileHeader">
