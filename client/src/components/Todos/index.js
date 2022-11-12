26fd9de13d4a1237b8529da5591246085bdc26db
Phillip <phillip.besse@gmail.com>
2022-11-12 17:10:51 -0500

    move QUERY_ME to todo index.js

--- client/src/components/Todos/index.js
+++ client/src/components/Todos/index.js
@@ -10,7 +10,7 @@ export default function Todos() {
   const { todo_loading, todo_data } = useQuery(QUERY_ALL_TODOS);
   const todos = todo_data?.todos || [];
 
-  const { me_loading, me_data } = useQuery(QUERY_ME);
+  const { loading: me_loading, data: me_data } = useQuery(QUERY_ME);
   const me_username = me_data?.me?.username;
   console.log(me_username);
 
